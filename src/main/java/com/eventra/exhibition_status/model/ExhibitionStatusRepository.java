60cf335920813cd15656c8254a2b14c884939b9a
L.W.Lian <bluebire40@gmail.com>
2025-09-09 01:11:11 +0800

    first commit add exhibitorVO & Repo

---
+++ src/main/java/com/eventra/exhibition_status/model/ExhibitionStatusRepository.java
@@ -0,0 +1,6 @@
+package com.eventra.exhibition_status.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExhibitionStatusRepository extends JpaRepository<ExhibitionStatusVO, Integer> {
+}
