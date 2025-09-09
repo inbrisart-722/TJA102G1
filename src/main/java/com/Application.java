cc508936bbb6f4fcc49cd2ed275dc9d501ac7764
L.W.Lian <bluebire40@gmail.com>
2025-09-09 17:00:41 +0800

    back-end css、js、html

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -2,12 +2,12 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+        System.out.println("Web Server? " + context.containsBean("webServerFactory"));
+    }
 }
