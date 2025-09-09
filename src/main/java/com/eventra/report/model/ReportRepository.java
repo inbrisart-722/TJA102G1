2536e714f252d73def9cf7a57ea4b39c38386f4d
P.C.Lu <lupc9405@gmail.com>
2025-09-09 16:58:46 +0800

    檢舉 VO Repository 上傳

---
+++ src/main/java/com/eventra/report/model/ReportRepository.java
@@ -0,0 +1,7 @@
+package com.eventra.report.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportRepository extends JpaRepository<ReportVO, Integer> {
+
+}
