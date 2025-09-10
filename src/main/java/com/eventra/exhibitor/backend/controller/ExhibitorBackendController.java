bbd8ec9f9039a7c651ea7db4ad68e5ea06f665a9
L.W.Lian <bluebire40@gmail.com>
2025-09-10 14:48:54 +0800

    補css及html檔，更改event_list裡的路徑

---
+++ src/main/java/com/eventra/exhibitor/backend/controller/ExhibitorBackendController.java
@@ -0,0 +1,15 @@
+package com.eventra.exhibitor.backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/back-end/exhibitor")
+public class ExhibitorBackendController {
+
+    @GetMapping("event_list")
+    public String enterBackendExhibitor(){
+        return "back-end/event_list";
+    }
+}
