25beef30165dddb8745f248883042a1b5e2cb819
P.C.Lu <lupc9405@gmail.com>
2025-09-11 14:59:24 +0800

    新增admin, exhibitions 頁面控制器

--- src/main/java/com/IndexController_inSpringBoot.java
+++ src/main/java/com/IndexController_inSpringBoot.java
@@ -1,10 +1,17 @@
 package com;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.eventra.favorite.model.FavoriteService;
 
 @Controller
+@RequestMapping("/front-end")
 public class IndexController_inSpringBoot {
 
     @GetMapping("/platform/index")
@@ -15,7 +22,21 @@ public class IndexController_inSpringBoot {
 
     @GetMapping("/back-end/create_event")
 	public String exhibitions(Model model) {
-		return "front-end/create_event"; // 對應 templates/platform/index.html
+		return "back-end/create_event"; // 對應 templates/back-end/create_event.html
 	}
 
+    @Autowired
+    private FavoriteService favSvc;
+    
+    @GetMapping("/admin")
+    public String adminPage(Model model) {
+        model.addAttribute("favList", favSvc.findFavoritesByMember(1));
+        return "front-end/admin";
+    }
+
+    @GetMapping("/exhibitions")
+    public String exhibitionsPage(Model model) {
+        model.addAttribute("favList", favSvc.findFavoritesByMember(1));
+        return "front-end/exhibitions";
+    }
 }
