30ef50ebd1c0426b14ac3192b27aecdfa6b5aa6a
Brian <brianchen890722@gmail.com>
2025-09-11 19:28:16 +0800

    cart 目前可送入 payment 剩下 payment thymeleaf 稍微渲染一下 就可繼續寫 admin 或 重送金流 的部分

--- src/main/java/com/IndexController_inSpringBoot.java
+++ src/main/java/com/IndexController_inSpringBoot.java
@@ -1,10 +1,14 @@
 package com;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.eventra.favorite.model.FavoriteService;
 
@@ -16,7 +20,6 @@ public class IndexController_inSpringBoot {
 	public String select_page(Model model) {
 		return "platform/index"; // 對應 templates/platform/index.html
 	}
-    
 
     @GetMapping("back-end/create_event")
 	public String exhibitions(Model model) {
@@ -43,8 +46,18 @@ public class IndexController_inSpringBoot {
     	return "front-end/cart";
     }
     
-    @GetMapping("front-end/payment")
-    public String paymentPage(Model model) {
+    @PostMapping("front-end/payment")
+    public String paymentPage(@RequestParam List<Integer> cartItemIds, Model model) {
+    	// 找到指定 cartItemDTOs
+    	System.out.println(cartItemIds);
+    	
+    	model.addAttribute("cartItemVOs", null);
     	return "front-end/payment";
     }
+    
+    @GetMapping("front-end/index")
+    public String index() {
+    	return "front-end/index";
+    }
+    
 }
