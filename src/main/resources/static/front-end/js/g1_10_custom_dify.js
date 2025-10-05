37ae08e6b51ab021a1030a9d8207bf0d15f30e9c
Brian <brianchen890722@gmail.com>
2025-10-05 13:52:11 +0800

    所有front-end頁面加上dify&websocket chat

---
+++ src/main/resources/static/front-end/js/g1_10_custom_dify.js
@@ -0,0 +1,24 @@
+// dify.ai ai chatbot
+window.difyChatbotConfig = {
+	token: '6dNmiwvbRPuqvJW7',
+	inputs: {
+		welcome_message: "您好，歡迎來到 Eventra，請詢問任何問題！"
+		// You can define the inputs from the Start node here
+		// key is the variable name
+		// e.g.
+		// name: "NAME"
+	},
+	layout: {
+		newChatButton: false, // 🔥 關閉「新的聊天」按鈕
+	},
+	systemVariables: {
+		// user_id: 'YOU CAN DEFINE USER ID HERE',
+		// conversation_id: 'YOU CAN DEFINE CONVERSATION ID HERE, IT MUST BE A VALID UUID',
+	},
+	userVariables: {
+		// avatar_url: 'YOU CAN DEFINE USER AVATAR URL HERE',
+		// name: 'YOU CAN DEFINE USER NAME HERE',
+	},
+}
+
+
