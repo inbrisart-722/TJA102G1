7e5d42830eb8c9a42dd2d596b5c350043d5fcb3f
Brian <brianchen890722@gmail.com>
2025-10-06 17:31:03 +0800

    更改 scheduler 印出訊息更完整

--- src/main/java/com/eventra/cart_item/scheduler/ExpiringCartItemLineBotScheduler.java
+++ src/main/java/com/eventra/cart_item/scheduler/ExpiringCartItemLineBotScheduler.java
@@ -19,13 +19,13 @@ public class ExpiringCartItemLineBotScheduler {
 	
 	@Scheduled(fixedRate = 30_000)
 	public void checkFiveMinutesLeft() {
-		System.out.println("ExpiringCartItemLineBotScheduler: checkFiveMinutesLeft");
+		System.out.println("-----每30秒掃: ExpiringCartItemLineBotScheduler: checkFiveMinutesLeft 找到所有4.93分鐘內過期且5分鐘內未通知過的用戶-----");
 		CART_ITEM_SERVICE.pushGlobalExpiringCartItem();
 	}
 	
 	@Scheduled(fixedRate = 45_000)
 	public void cleanupGlobalExp(){
-		System.out.println("ExpiringCartItemLineBotScheduler: cleanupGlobalExp");
+		System.out.println("-----每45秒掃: ExpiringCartItemLineBotScheduler: cleanupGlobalExp 調整 cart:items:globalExp 裡面的會員過期時間取出最快過期那筆-----");
 		CART_ITEM_REDIS_REPO.cleanupGlobalExp();
 	}
 }
